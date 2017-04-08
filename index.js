70dfd0e61f79d659007d2b0f4daaf5261741f382
Yohan <wyohan@gmail.com>
2017-04-08 15:13:27 -0300

    ok

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json()); // for parsing application/json
 MongoClient.connect('mongodb://dba:m4st3r@ds149278.mlab.com:49278/demo', function (err, db) {
   if (err) throw err
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
